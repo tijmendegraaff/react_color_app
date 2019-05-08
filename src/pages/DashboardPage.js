32396334802983a0e44662ee05b08b469eff9181
Tijmen de Graaff <tijmen76@gmail.com>
2019-05-08 15:14:08 +0200

    add helper function to calculate colors

--- src/pages/DashboardPage.js
+++ src/pages/DashboardPage.js
@@ -2,11 +2,15 @@ import React from 'react';
 import Palette from '../components/Palette';
 
 import seedColors from '../constants/seedColors';
+import generatePalette from '../helpers/colorHelpers';
 
-const DashboardPage = () => (
-  <div>
-    <Palette {...seedColors[2]} />
-  </div>
-);
+const DashboardPage = () => {
+  console.log(generatePalette(seedColors[2]));
+  return (
+    <div>
+      <Palette {...seedColors[2]} />
+    </div>
+  );
+};
 
 export default DashboardPage;
