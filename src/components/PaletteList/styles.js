b3944cc04ec49d579a103d2ba7ca4c28c0c50a9d
Tijmen de Graaff <tijmen76@gmail.com>
2019-05-15 23:52:43 +0200

    make platte list responsive

--- src/components/PaletteList/styles.js
+++ src/components/PaletteList/styles.js
@@ -1,3 +1,5 @@
+import { breakPoint } from '../../constants/cssConstants';
+
 const styles = {
   paletteListWrapper: {
     backgroundColor: 'blue',
@@ -12,6 +14,12 @@ const styles = {
     alignItems: 'flex-start',
     flexDirection: 'column',
     flexWrap: 'wrap',
+    [breakPoint.down('xl')]: {
+      width: '80%',
+    },
+    [breakPoint.down('xs')]: {
+      width: '75%',
+    },
   },
   paletteListNav: {
     display: 'flex',
@@ -29,6 +37,13 @@ const styles = {
     display: 'grid',
     gridTemplateColumns: 'repeat(3, 30%)',
     gridGap: '5%',
+    [breakPoint.down('md')]: {
+      gridTemplateColumns: 'repeat(2, 50%)',
+    },
+    [breakPoint.down('xs')]: {
+      gridTemplateColumns: 'repeat(1, 100%)',
+      gridGap: '1rem',
+    },
   },
 };
 
--- src/constants/cssConstants.js
+++ src/constants/cssConstants.js
@@ -8,6 +8,7 @@ export const breakPoint = {
       sm: '767.98px',
       md: '991.98px',
       lg: '1199.98px',
+      xl: '1600px',
     };
     return `@media (max-width: ${sizes[size]})`;
   },
