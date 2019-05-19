510e088d50ad74730f6d97d9f87564158b0c489e
Tijmen de Graaff <tijmen76@gmail.com>
2019-05-20 01:04:20 +0200

    implement login and register mutation component

--- src/components/LoginForm/LoginForm.js
+++ src/components/LoginForm/LoginForm.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
+import { Mutation } from 'react-apollo';
 import { withStyles } from '@material-ui/styles';
+import LOGIN_MUTATION from '../../graphql/mutations/login_mutation';
+import REGISTER_MUTATION from '../../graphql/mutations/register_mutation';
 import FormField from '../FormField/FormField';
+import { AUTH_TOKEN } from '../../constants/constants';
 import styles from './styles';
 
 class LoginForm extends Component {
@@ -12,21 +16,14 @@ class LoginForm extends Component {
       lastName: '',
       email: '',
       password: '',
-      passwordConformation: '',
+      passwordConfirmation: '',
     };
     this.onChange = this.onChange.bind(this);
-    this.onSubmit = this.onSubmit.bind(this);
     this.setAuthState = this.setAuthState.bind(this);
   }
 
   onChange(e) {
     this.setState({ [e.target.name]: e.target.value });
-  }
-
-  onSubmit(e) {
-    const { email, password } = this.state;
-    e.preventDefault();
-    console.log(this.state);
   }
 
   setAuthState(e) {
@@ -37,97 +34,140 @@ class LoginForm extends Component {
       lastName: '',
       email: '',
       password: '',
-      passwordConformation: '',
+      passwordConfirmation: '',
     });
   }
 
+  saveToken = async (data) => {
+    const { auth } = this.state;
+    const loginState = auth === 'login';
+    const { history } = this.props;
+    const { token } = loginState ? data.createSession : data.createUser;
+    await localStorage.setItem(AUTH_TOKEN, token);
+    history.push('/');
+  };
+
   render() {
     const { classes } = this.props;
-    const {
-      auth, email, password, firstName, lastName, passwordConformation,
-    } = this.state;
-    const renderLogin = auth === 'login';
-    return (
-      <div className={classes.container}>
-        <div className={classes.formContainer}>
-          <div className={classes.formWrapper}>
-            {renderLogin ? (
-              <>
-                <h3 className={classes.title}>Login Page</h3>
-                <form onSubmit={this.onSubmit}>
-                  <FormField name="email" onChange={this.onChange} type="text" value={email} />
-                  <FormField
-                    name="password"
-                    onChange={this.onChange}
-                    type="password"
-                    value={password}
-                  />
-                  <div className={classes.submitButtonContainer}>
-                    <button className={classes.submitButton} type="submit">
-                      Login
-                    </button>
-                  </div>
-                </form>
-              </>
-            ) : (
-              <>
-                <h3 className={classes.title}>Register</h3>
-                <form onSubmit={this.onSubmit}>
-                  <FormField
-                    name="firstName"
-                    onChange={this.onChange}
-                    type="text"
-                    value={firstName}
-                  />
-                  <FormField
-                    name="lastName"
-                    onChange={this.onChange}
-                    type="text"
-                    value={lastName}
-                  />
-                  <FormField name="email" onChange={this.onChange} type="text" value={email} />
-                  <FormField
-                    name="password"
-                    onChange={this.onChange}
-                    type="password"
-                    value={password}
-                  />
-                  <FormField
-                    name="passwordConformation"
-                    onChange={this.onChange}
-                    type="password"
-                    value={passwordConformation}
-                  />
-                  <div className={classes.submitButtonContainer}>
-                    <button type="submit" className={classes.submitButton}>
-                      Register
-                    </button>
-                  </div>
-                </form>
-              </>
-            )}
 
-            <div className={classes.buttonContainer}>
-              <button
-                name="login"
-                type="button"
-                className={classes.button}
-                onClick={this.setAuthState}
-              >
-                Login
-              </button>
-              <button
-                name="register"
-                type="button"
-                className={classes.button}
-                onClick={this.setAuthState}
-              >
-                Register
-              </button>
+    const {
+      auth, email, password, firstName, lastName, passwordConfirmation,
+    } = this.state;
+
+    const loginState = auth === 'login';
+
+    return (
+      <Mutation
+        mutation={loginState ? LOGIN_MUTATION : REGISTER_MUTATION}
+        variables={
+          loginState
+            ? { input: { email, password } }
+            : {
+              input: {
+                firstName,
+                lastName,
+                email,
+                password,
+                passwordConfirmation,
+              },
+            }
+        }
+        onCompleted={data => this.saveToken(data)}
+        onError={error => console.log(error.message)}
+      >
+        {(mutation, { loading }) => (
+          <div className={classes.container}>
+            <div className={classes.formContainer}>
+              <div className={classes.formWrapper}>
+                {loginState ? (
+                  <>
+                    <h3 className={classes.title}>Login Page</h3>
+                    <form
+                      onSubmit={(e) => {
+                        e.preventDefault();
+                        mutation();
+                      }}
+                    >
+                      <FormField name="email" onChange={this.onChange} type="text" value={email} />
+                      <FormField
+                        name="password"
+                        onChange={this.onChange}
+                        type="password"
+                        value={password}
+                      />
+                      <div className={classes.submitButtonContainer}>
+                        <button disabled={loading} className={classes.submitButton} type="submit">
+                          Login
+                        </button>
+                      </div>
+                    </form>
+                  </>
+                ) : (
+                  <>
+                    <h3 className={classes.title}>Register</h3>
+                    <form
+                      onSubmit={(e) => {
+                        e.preventDefault();
+                        mutation();
+                      }}
+                    >
+                      <FormField
+                        name="firstName"
+                        onChange={this.onChange}
+                        type="text"
+                        value={firstName}
+                      />
+                      <FormField
+                        name="lastName"
+                        onChange={this.onChange}
+                        type="text"
+                        value={lastName}
+                      />
+                      <FormField name="email" onChange={this.onChange} type="text" value={email} />
+                      <FormField
+                        name="password"
+                        onChange={this.onChange}
+                        type="password"
+                        value={password}
+                      />
+                      <FormField
+                        name="passwordConfirmation"
+                        onChange={this.onChange}
+                        type="password"
+                        value={passwordConfirmation}
+                      />
+                      <div className={classes.submitButtonContainer}>
+                        <button disabled={loading} type="submit" className={classes.submitButton}>
+                          Register
+                        </button>
+                      </div>
+                    </form>
+                  </>
+                )}
+
+                <div className={classes.buttonContainer}>
+                  <button
+                    name="login"
+                    type="button"
+                    className={classes.button}
+                    onClick={this.setAuthState}
+                  >
+                    Login
+                  </button>
+                  <button
+                    name="register"
+                    type="button"
+                    className={classes.button}
+                    onClick={this.setAuthState}
+                  >
+                    Register
+                  </button>
+                </div>
+              </div>
             </div>
           </div>
-        </div>
-      </div>
+        )}
+      </Mutation>
     );
   }
 }
